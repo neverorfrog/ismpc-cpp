10a72c7a8aa61d1e49f7a313eeda30b2ab7776ef
neverorfrog <97flavio.maiorana@gmail.com>
2024-12-10 17:25:34 +0100

    moved folder and added readme

--- include/tools/systemvars.h
+++ include/tools/systemvars.h
@@ -5,8 +5,8 @@
 namespace ismpc {
 
 constexpr char PROJECT_VERSION[] = "0.1";
-constexpr char CMAKE_SOURCE_DIR[] = "/home/neverorfrog/code/intrinsically-stable-mpc";
-constexpr char CMAKE_BINARY_DIR[] = "/home/neverorfrog/code/intrinsically-stable-mpc/build";
+constexpr char CMAKE_SOURCE_DIR[] = "/home/neverorfrog/code/ismpc-cpp";
+constexpr char CMAKE_BINARY_DIR[] = "/home/neverorfrog/code/ismpc-cpp/build";
 constexpr char NAO_DESCRIPTION_PATH[] = "";
 
 }  // namespace ismpc
